ee980ba6b0e8987f79b36f13b472ea3c6d16a063
bibekshah220 <bibekshah425@gmail.com>
2026-06-06 22:30:23 +0545

    feat: implement pages for adding address, viewing all products, cart functionality, order history, and product details

---
+++ app/seller/layout.jsx
@@ -0,0 +1,16 @@
+'use client'
+import Navbar from '@/components/seller/Navbar'
+import Sidebar from '@/components/seller/Sidebar'
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <Navbar />
+      <div className='flex w-full'>
+        <Sidebar />
+        {children}
+      </div>
+    </div>
+  )
+}
+
+export default Layout
