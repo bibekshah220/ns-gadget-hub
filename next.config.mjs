69f86d89c0c326a58963edc96f682c26fd8671e2
bibekshah220 <bibekshah425@gmail.com>
2026-06-06 22:28:34 +0545

    chore: initialize project with Next.js, Tailwind CSS, and ESLint configuration
    - Added package.json with project metadata and dependencies
    - Created postcss.config.mjs for PostCSS configuration with Tailwind CSS
    - Added tailwind.config.mjs for Tailwind CSS configuration

---
+++ next.config.mjs
@@ -0,0 +1,25 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    outputFileTracingRoot: __dirname,
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'res.cloudinary.com',
+                pathname: '**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'raw.githubusercontent.com',
+                pathname: '**',
+            },
+        ],
+    },
+};
+
+export default nextConfig;
