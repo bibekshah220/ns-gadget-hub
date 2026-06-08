95eda50df00c1f5225e6f204461178f5a9d78b75
bibekshah220 <bibekshah425@gmail.com>
2026-06-08 22:37:34 +0545

    feat: add syncUserCreation function to handle user creation events from Clerk

--- config/inngest.js
+++ config/inngest.js
@@ -3,3 +3,19 @@ import { Inngest } from "inngest";
 
 export const inngest = new Inngest({ id: "NS-Gadget-Hub" });
 
+/**
+ * inggest function to save user data to a database
+ */
+
+export const syncUserCreation = inngest.createFunction(
+    {
+  id: "sync-user-from-clerk",
+     },
+     {
+        event: "clerk/user.created",
+     },
+     async ({ event }) => {
+        const { _id, email_addresses, first_name, last_name, image_url } = event.data;
+     }
+);
+ 
