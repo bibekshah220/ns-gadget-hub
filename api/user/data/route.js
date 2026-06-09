438b324981be10551e0bc71fe9f588557577f035
bibekshah220 <bibekshah425@gmail.com>
2026-06-09 23:02:01 +0545

    fix: ensure database connection is established in GET request handler

--- api/user/data/route.js
+++ api/user/data/route.js
@@ -1,6 +1,7 @@
 export async function GET(request) {
     try{
 const {userId} = getAuth(request);
+await connectDB();
 
     }catch (error) {
         return new Response(JSON.stringify({error: "Unauthorized"}), {status: 401});
