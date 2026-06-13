165a7279e3f2cdbccd6711154e441f16967ad7f8
bibekshah220 <bibekshah425@gmail.com>
2026-06-13 23:46:44 +0545

    fix: correct getAuth usage in seller list GET route

--- api/product/sellerList/route.js
+++ api/product/sellerList/route.js
@@ -1,9 +1,11 @@
 import { getAuth } from "@clerk/nextjs/server";
+import { request } from "node:http";
 
 export async function GET() {
     try{
 
-const {userId} = getAuth
+const {userId} = getAuth(request)
+const isSeller = authSeller(userId)
 
 
 
