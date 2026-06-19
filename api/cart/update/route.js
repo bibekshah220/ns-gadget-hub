1dc82553e32eddc56d139c303b37f187bdb3dd38
bibekshah220 <bibekshah425@gmail.com>
2026-06-19 23:59:35 +0545

    refactor: simplify import statements and prepare for Next.js response handling in cart update route

--- api/cart/update/route.js
+++ api/cart/update/route.js
@@ -1,9 +1,7 @@
 import connectToDatabase from "@/config/db";
 import User from "@/models/User";
 import { getAuth } from "@clerk/nextjs/dist/types/server";
-
-
-
+import { NextResponse } from "next/server";
 
 export async function POST(req) {
     try{
