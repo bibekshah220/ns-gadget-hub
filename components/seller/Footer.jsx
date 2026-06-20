7ff606bc9a6b51157cbd20e0d60ed37c57fb3ca9
bibekshah220 <bibekshah425@gmail.com>
2026-06-21 01:23:11 +0545

    feat: update copyright year in Footer component to 2026

--- components/seller/Footer.jsx
+++ components/seller/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
         </span>
         <div className="hidden md:block h-7 w-px bg-gray-500/60"></div>
         <p className="py-4 text-center text-xs md:text-sm text-gray-500">
-          Copyright 2025 © N.S Gadget Hub All Rights Reserved.
+          Copyright 2026 © N.S Gadget Hub All Rights Reserved.
         </p>
       </div>
       <div className="flex items-center gap-3">
