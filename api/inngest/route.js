4d7efcbe78296e28d37f0d3954d5df05c997ed30
bibekshah220 <bibekshah425@gmail.com>
2026-06-21 14:45:37 +0545

    refactor: reorganize import statements for better readability

--- api/inngest/route.js
+++ api/inngest/route.js
@@ -1,6 +1,11 @@
 import { serve } from "inngest/next";
-import { inngest } from "@/../config/inngest";
-import { syncUserCreation, syncUserDeletion, syncUserUpdate, createUserOrder } from "@/config/inngest";
+import {
+  inngest,
+  syncUserCreation,
+  syncUserUpdate,
+  syncUserDeletion,
+  createUserOrder,
+} from "@/config/inngest";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
